970cbd6b203a8844ae6a7e65eacf619ec643984b
Dev_ <89705058+hyperdownload@users.noreply.github.com>
2024-09-06 14:44:24 -0300

    Cambios

--- Utils/database.py
+++ Utils/database.py
@@ -2,17 +2,18 @@ import sqlite3
 import hashlib
 
 class Product:
-    def __init__(self, name:str, price:int, brand:str, size:any, image:any, description:str, branch_id:int, image_path=None):
+    def __init__(self, name:str, price:int, brand:str, size:any, image:any, description:str, branch_name:str, stock:int, image_path=None):
         self.name = name
         self.price = price
         self.brand = brand
         self.size = size
+        self.stock = stock
         if image_path:
             self.image = self.convert_image_to_binary(image_path)
         else:
             self.image = image  # Asume que image es un binario si image_path no está presente
         self.description = description
-        self.branch_id = branch_id
+        self.branch_name = branch_name
 
     def convert_image_to_binary(self, image_path)->bytearray:
         with open(image_path, 'rb') as file:
@@ -26,9 +27,10 @@ class Product:
             price=row[2],
             brand=row[3],
             size=row[4],
-            image=row[5],  # Pasa la imagen directamente desde la fila
+            image=row[5],  
             description=row[6],
-            branch_id=row[7]
+            branch_name=row[7],
+            stock=row[8]
         )
 
 dataBasePath = './Bd/clothing_store.db'
@@ -41,16 +43,18 @@ def create_database()->None:
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Branches (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
+        name TEXT NOT NULL UNIQUE,
         address TEXT NOT NULL
     )
     ''')
+
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Roles (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL
     )
     ''')
+
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -64,6 +68,7 @@ def create_database()->None:
         FOREIGN KEY (branch_id) REFERENCES Branches(id)
     )
     ''')
+
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Products (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -73,60 +78,68 @@ def create_database()->None:
         size TEXT NOT NULL,
         image BLOB,
         description TEXT,
-        branch_id INTEGER NOT NULL,
-        FOREIGN KEY (branch_id) REFERENCES Branches(id)
+        branch_name TEXT NOT NULL,
+        stock INTEGER DEFAULT 0,
+        FOREIGN KEY (branch_name) REFERENCES Branches(name)
     )
     ''')
+
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Sales (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         product_id INTEGER NOT NULL,
         user_id INTEGER NOT NULL,
-        branch_id INTEGER NOT NULL,
+        branch_name TEXT NOT NULL,
         quantity INTEGER NOT NULL,
         date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (product_id) REFERENCES Products(id),
         FOREIGN KEY (user_id) REFERENCES Users(id),
-        FOREIGN KEY (branch_id) REFERENCES Branches(id)
+        FOREIGN KEY (branch_name) REFERENCES Branches(name)
     )
     ''')
+
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Restocks (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         product_id INTEGER NOT NULL,
         user_id INTEGER NOT NULL,
-        branch_id INTEGER NOT NULL,
+        branch_name TEXT NOT NULL,
         quantity INTEGER NOT NULL,
         date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (product_id) REFERENCES Products(id),
         FOREIGN KEY (user_id) REFERENCES Users(id),
-        FOREIGN KEY (branch_id) REFERENCES Branches(id)
+        FOREIGN KEY (branch_name) REFERENCES Branches(name)
     )
     ''')
+
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Logs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         action TEXT NOT NULL,
         user_id INTEGER,
         product_id INTEGER,
-        branch_id INTEGER,
+        branch_name TEXT,
         quantity INTEGER,
         date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (user_id) REFERENCES Users(id),
         FOREIGN KEY (product_id) REFERENCES Products(id),
-        FOREIGN KEY (branch_id) REFERENCES Branches(id)
+        FOREIGN KEY (branch_name) REFERENCES Branches(name)
     )
     ''')
 
     # Inserta roles por default
-    cursor.execute('INSERT OR IGNORE INTO Roles (name) VALUES (?)', ('Normal User',))
-    cursor.execute('INSERT OR IGNORE INTO Roles (name) VALUES (?)', ('Admin',))
-    cursor.execute('INSERT OR IGNORE INTO Roles (name) VALUES (?)', ('General Admin',))
+    #cursor.execute('INSERT OR IGNORE INTO Roles (name) VALUES (?)', ('Normal User',))
+    #cursor.execute('INSERT OR IGNORE INTO Roles (name) VALUES (?)', ('Admin',))
+    #cursor.execute('INSERT OR IGNORE INTO Roles (name) VALUES (?)', ('General Admin',))
+
+    # Inserta sucursales de ejemplo
+    #cursor.execute('INSERT OR IGNORE INTO Branches (name, address) VALUES (?, ?)', ('Main Branch', '123 Main St'))
+    #cursor.execute('INSERT OR IGNORE INTO Branches (name, address) VALUES (?, ?)', ('West Branch', '456 West St'))
 
     conn.commit()
     conn.close()
 
-    print("Database created successfully.")
+    print("Database creada.")
 
 def register_user(name, email, password, role_id, branch_id)->None:
     conn = sqlite3.connect(dataBasePath)
@@ -134,7 +147,7 @@ def register_user(name, email, password, role_id, branch_id)->None:
     
     cursor.execute('SELECT id FROM Users WHERE email = ?', (email,))
     if cursor.fetchone() is not None:
-        print(f"Error: A user with the email '{email}' already exists.")
+        print(f"Error: ya existe un usuario con este email.")
         conn.close()
         return
     
@@ -148,7 +161,7 @@ def register_user(name, email, password, role_id, branch_id)->None:
     conn.commit()
     conn.close()
 
-    print(f"User {name} registered successfully.")
+    print(f"User {name} registrado.")
 
 def get_user_id(email:str)->None:
     '''
@@ -171,63 +184,92 @@ def add_product(product)->None:
     cursor = conn.cursor()
 
     cursor.execute('''
-    INSERT INTO Products (name, price, brand, size, image, description, branch_id)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
-    ''', (product.name, product.price, product.brand, product.size, product.image, product.description, product.branch_id))
+    INSERT INTO Products (name, price, brand, size, image, description, branch_name, stock)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    ''', (product.name, product.price, product.brand, product.size, product.image, product.description, product.branch_name, product.stock))
     
     conn.commit()
     conn.close()
 
     print(f"Product {product.name} added successfully.")
 
-def record_sale(product_id:int, user_id:int, branch_id:int, quantity:int)->None:
+def record_sale(product_id:int, user_id:int, branch_name:str, quantity:int)->None:
     '''
-    Guarda una venta de un producto
+    Guarda una venta de un producto y reduce el stock del producto.
     '''
     conn = sqlite3.connect(dataBasePath)
     cursor = conn.cursor()
-    
-    cursor.execute('''
-    INSERT INTO Sales (product_id, user_id, branch_id, quantity)
-    VALUES (?, ?, ?, ?)
-    ''', (product_id, user_id, branch_id, quantity))
-    
-    cursor.execute('''
-    INSERT INTO Logs (action, user_id, product_id, branch_id, quantity)
-    VALUES (?, ?, ?, ?, ?)
-    ''', ('Sale', user_id, product_id, branch_id, quantity))
 
-    conn.commit()
+    # Verifica el stock actual
+    cursor.execute('SELECT stock FROM Products WHERE id = ?', (product_id,))
+    current_stock = cursor.fetchone()[0]
+
+    if current_stock >= quantity:
+        # Registra la venta
+        cursor.execute('''
+        INSERT INTO Sales (product_id, user_id, branch_name, quantity)
+        VALUES (?, ?, ?, ?)
+        ''', (product_id, user_id, branch_name, quantity))
+
+        # Reduce el stock
+        new_stock = current_stock - quantity
+        cursor.execute('UPDATE Products SET stock = ? WHERE id = ?', (new_stock, product_id))
+
+        # Registra la acción en los logs
+        cursor.execute('''
+        INSERT INTO Logs (action, user_id, product_id, branch_name, quantity)
+        VALUES (?, ?, ?, ?, ?)
+        ''', ('Sale', user_id, product_id, branch_name, quantity))
+
+        conn.commit()
+        print(f"Venta guardada .Stock: {new_stock}")
+    else:
+        print("Error: No hay suficiente stock.")
+
     conn.close()
-
-    print("Sale recorded successfully.")
-
-def record_restock(product_id:int, user_id:int, branch_id:int, quantity:int)->None:
-    """Guarda el reestock de un producto
-
-        :param product_id: int, 
-        user_id (_type_): _description_
-        branch_id (_type_): _description_
-        quantity (_type_): _description_
+    
+def get_products_out_of_stock()->dict:
     """
-    
+    Recupera los productos que no tienen stock disponible.
+    """
     conn = sqlite3.connect(dataBasePath)
     cursor = conn.cursor()
-    
+
     cursor.execute('''
-    INSERT INTO Restocks (product_id, user_id, branch_id, quantity)
+    SELECT p.id, p.name, b.name, p.price, p.stock FROM Products p
+    JOIN Brands b ON p.brand_id = b.id
+    WHERE p.stock = 0
+    ''')
+    
+    products_out_of_stock = cursor.fetchall()
+    conn.close()
+
+    return products_out_of_stock
+
+def record_restock(product_id:int, user_id:int, branch_name:str, quantity:int)->None:
+    '''
+    Guarda el reestock de un producto
+    '''
+    conn = sqlite3.connect(dataBasePath)
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT stock FROM Products WHERE id = ?', (product_id,))
+    current_stock = cursor.fetchone()[0]
+    new_stock = current_stock + quantity
+
+    cursor.execute('UPDATE Products SET stock = ? WHERE id = ?', (new_stock, product_id))
+    cursor.execute('''
+    INSERT INTO Restocks (product_id, user_id, branch_name, quantity)
     VALUES (?, ?, ?, ?)
-    ''', (product_id, user_id, branch_id, quantity))
-    
+    ''', (product_id, user_id, branch_name, quantity))
+
     cursor.execute('''
-    INSERT INTO Logs (action, user_id, product_id, branch_id, quantity)
+    INSERT INTO Logs (action, user_id, product_id, branch_name, quantity)
     VALUES (?, ?, ?, ?, ?)
-    ''', ('Restock', user_id, product_id, branch_id, quantity))
+    ''', ('Restock', user_id, product_id, branch_name, quantity))
 
     conn.commit()
     conn.close()
-
-    print("Restock recorded successfully.")
 
 def get_products_in_stock()->dict:
     """
@@ -247,19 +289,20 @@ def get_products_in_stock()->dict:
     return products
 
 def get_user_details(user_id:int)->tuple:
-    """
-    Recupera el email, rol y contraseña de un usuario basado en su ID.
-    """
+    '''
+    Obtiene detalles de un usuario junto con su sucursal asociada
+    '''
     conn = sqlite3.connect(dataBasePath)
     cursor = conn.cursor()
-    
+
     cursor.execute('''
-    SELECT Users.email, Roles.name as role, Users.password
+    SELECT Users.email, Roles.name as role, Users.password, Branches.name as branch
     FROM Users
     JOIN Roles ON Users.role_id = Roles.id
+    JOIN Branches ON Users.branch_id = Branches.id
     WHERE Users.id = ?
     ''', (user_id,))
-    
+
     user_details = cursor.fetchone()
     conn.close()
     
@@ -307,8 +350,8 @@ if __name__ == "__main__":
     register_user('Jane Smith', 'jane@example.com', 'admin456', 2, 2)
     register_user('Carlos Torres', 'carlos@example.com', 'superadmin789', 3, 3)
 
-    product1 = Product('White T-shirt', 19.99, 'Brand X', 'M', None, 'White cotton t-shirt', 1, image_path='image1.jpg')
-    product2 = Product('Blue Jeans', 39.99, 'Brand Y', 'L', None, 'Blue denim jeans', 2, image_path='image1.jpg')
+    product1 = Product('White T-shirt', 19.99, 'Brand X', 'M', None, 'White cotton t-shirt', "Un local", image_path='image1.jpg', stock=10)
+    product2 = Product('Blue Jeans', 39.99, 'Brand Y', 'L', None, 'Blue denim jeans', "dos", image_path='image1.jpg', stock=10)
 
     add_product(product1)
     add_product(product2)
